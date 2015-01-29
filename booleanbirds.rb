5a39d5363a7e951fbad75be9d738f8762e4f353c
Sia <siakaramalegos@gmail.com>
2015-01-29 10:10:21 -0600

    Add arrays with awesome song generator

---
+++ booleanbirds.rb
@@ -0,0 +1,38 @@
+puts "Welcome to Boolean Birds!!"
+
+# # ----------------------------- For loops
+# puts "1 bottle of wine off the wall"
+
+# for n in 2..5
+#   puts "#{n} bottles of wine off the wall"
+# end
+
+# # ------------------------------ While loops
+# x = 1
+# while x <= 10
+#   puts "#{x} is the loneliest number"
+#   if x == 10
+#     puts "We have almost exited the loop!"
+#   end
+#   x += 1
+# end
+
+# puts "We have really exited the loop."
+
+# # ----------------------------- Times do loop
+# x = 5
+
+# x.times do |i|
+#   puts "We are at number #{i+1}!?!?"
+# end
+
+# # ------------------------------ Inline for loops
+# # These two loops do the same thing
+# for n in 1..5
+#   puts "We are number #{n}!!"
+# end
+
+# 1.upto(5){|n| puts "We are number #{n}!!!!!!"}
+
+# --------------------------------- Arrays
+
