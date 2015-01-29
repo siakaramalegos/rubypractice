90253773aff21dbe8f47149e536d783e63a3b780
Sia <siakaramalegos@gmail.com>
2015-01-29 11:16:10 -0600

    Updated arrays.rb song for comments and corrected username.length

--- arrays.rb
+++ arrays.rb
@@ -1,3 +1,4 @@
+# This method creates the song array
 def awesome (username_array, username)
   awesome_array = []
   awesome_array.push(username + " " + username_array[0])
@@ -9,19 +10,25 @@ def awesome (username_array, username)
   awesome_array
 end
 
+# This method creates the array of names that start with different letters
 def names username
+  # Array of first letters for funny names
   letters_array = ["B", "F", "M"]
   names_array = []
 
+  # Looping over each of the letters in the letters array
   for letters in letters_array
-    tempname=letters + username [1,username.length+1]
+    tempname=letters + username [1,username.length]
     names_array.push(tempname)
   end
 
+  # Don't forget to put the thing to be returned last!
   names_array
 end
 
 name = "Sia"
+# This says do the awesome method using the variables (names method on name)
+# and name.
 puts awesome(names(name), name)
 
 puts "\nWelcome to my song generator! What is your name?"
