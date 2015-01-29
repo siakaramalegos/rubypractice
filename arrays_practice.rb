3fdadbaf7bd3f2d6f19b3eb2689765316658126f
Sia <siakaramalegos@gmail.com>
2015-01-29 11:26:14 -0600

    Add arrays practice doc on looping

---
+++ arrays_practice.rb
@@ -0,0 +1,22 @@
+# scores = [100,5,7,309]
+# counter = 0
+# sum = 0
+
+# while counter < scores.length
+#   sum += scores[counter]
+#   counter += 1
+# end
+
+# puts "The total is #{sum}!"
+
+# ===============================================
+
+scores = [122,4,5,322]
+sum = 0
+
+scores.each do |score|
+  sum += score
+end
+
+puts "The total is #{sum}!"
+
