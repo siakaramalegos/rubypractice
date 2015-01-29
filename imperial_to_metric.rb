05388af6051adbc5bd5dbd9c62d3f25630fc35f4
Sia <siakaramalegos@gmail.com>
2015-01-29 08:25:45 -0600

    Add ifs to imperial to metric

--- imperial_to_metric.rb
+++ imperial_to_metric.rb
@@ -13,9 +13,25 @@ def get_height
 
   puts "What are the remaining inches?"
   height_inches = gets.chomp.to_i
+  if height_inches > 11
+    puts "Did you really mean #{height_inches}? (yes or no)"
+    confirm = gets.chomp.to_s.downcase
+    if confirm == "no"
+      get_height
+    end
+  end
 
-  # This last line will automatically return this value for the method
-  (height_feet*12 + height_inches)
+  total_inches = (height_feet*12 + height_inches)
+
+  if total_inches.zero?
+    puts "Are you real because your height is zero.  Please try again!"
+    get_height
+  elsif total_inches >=72
+    puts "Oh you're a tall glass of water."
+  else
+    puts "Thanks!"
+  end
+  total_inches
 end
 
 def get_weight
@@ -27,18 +43,13 @@ my_name = get_name
 
 total_height_inches = get_height
 
-if total_height_inches.zero?
-  puts "Are you real because your height is zero.  Please try again!"
-  total_height_inches = get_height
-elsif total_height_inches >=72
-  puts "Oh you're a tall glass of water."
-end
-
 height_cm = total_height_inches*2.54
 
 weight_pounds = get_weight
 if weight_pounds <= 125
   puts "You need to eat more!"
+else
+  puts "Thanks!"
 end
 
 weight_kg = weight_pounds*0.43592
